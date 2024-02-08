6cab7a4b72e38e3000101a77c388e2b32968e77c
Ferre-89 <ferre171717@gmail.com>
2024-02-08 14:56:18 -0300

    Navigation detail screen

--- app/src/main/java/com/example/profilecardlayout/MainActivity.kt
+++ app/src/main/java/com/example/profilecardlayout/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -41,13 +42,18 @@ import com.codingtroops.profilecardlayout.userProfileList
 import com.example.profilecardlayout.ui.theme.MyTheme
 import com.example.profilecardlayout.ui.theme.colorsScheme
 import com.example.profilecardlayout.ui.theme.shapeScheme
+import androidx.compose.ui.unit.Dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyTheme(dynamicColor = false) {
-                MainScreen()
+               UsersApplication()
             }
         }
     }
@@ -55,15 +61,51 @@ class MainActivity : ComponentActivity() {
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun MainScreen(userProfiles: List<UserProfile> = userProfileList) {
+fun UserListScreen(
+    userProfiles: List<UserProfile> = userProfileList, navController: NavHostController?
+) {
     Scaffold(topBar = { AppBar() }) {
         Surface(
             modifier = Modifier.fillMaxSize()
         ) {
             LazyColumn {
                 items(userProfiles) { userProfile ->
-                    ProfileCard(userProfile = userProfile)
+                    ProfileCard(userProfile = userProfile){
+                        navController?.navigate("user_deatils")
+                    }
                 }
+            }
+        }
+    }
+}
+
+@Composable
+fun UsersApplication(userProfiles: List<UserProfile> = userProfileList) {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "users_list") {
+        composable("users_list") {
+            UserListScreen(userProfileList, navController)
+        }
+        composable("user_deatils") {
+            UserProfileDetailsPreview()
+        }
+    }
+}
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun UserPrifleDetailsScreen(userProfile: UserProfile) {
+    Scaffold(topBar = { AppBar() }) {
+        Surface(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+                ProfilePicture(userProfile.pictureUrl, userProfile.status, 248.dp)
+                ProfileContent(userProfile.name, userProfile.status, Alignment.CenterHorizontally)
             }
         }
     }
@@ -81,12 +123,13 @@ fun AppBar() {
 }
 
 @Composable
-fun ProfileCard(userProfile: UserProfile) {
+fun ProfileCard(userProfile: UserProfile, clickAction: () -> Unit) {
     Card(
         modifier = Modifier
             .padding(top = 8.dp, bottom = 4.dp, start = 16.dp, end = 16.dp)
             .fillMaxWidth()
-            .wrapContentHeight(align = Alignment.Top),
+            .wrapContentHeight(align = Alignment.Top)
+            .clickable { clickAction.invoke() },
         elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
         colors = CardDefaults.cardColors(containerColor = Color.White),
         shape = MaterialTheme.shapeScheme.medium,
@@ -97,15 +140,15 @@ fun ProfileCard(userProfile: UserProfile) {
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Start
         ) {
-            ProfilePicture(userProfile.pictureUrl, userProfile.status)
-            ProfileContent(userProfile.name, userProfile.status)
+            ProfilePicture(userProfile.pictureUrl, userProfile.status, 72.dp)
+            ProfileContent(userProfile.name, userProfile.status, Alignment.Start)
         }
     }
 }
 
 
 @Composable
-fun ProfilePicture(pictureUrl: String, onlineStatus: Boolean) {
+fun ProfilePicture(pictureUrl: String, onlineStatus: Boolean, imageSize: Dp) {
     Card(
         shape = CircleShape,
         border = BorderStroke(
@@ -119,7 +162,7 @@ fun ProfilePicture(pictureUrl: String, onlineStatus: Boolean) {
         AsyncImage(
             model = pictureUrl,
             modifier = Modifier
-                .size(72.dp)
+                .size(imageSize)
                 .clip(CircleShape),
             contentDescription = "Profile picture description"
         )
@@ -127,30 +170,44 @@ fun ProfilePicture(pictureUrl: String, onlineStatus: Boolean) {
 }
 
 @Composable
-fun ProfileContent(userName: String, onlineStatus: Boolean) {
-    Column(modifier = Modifier.padding(8.dp)) {
+fun ProfileContent(userName: String, onlineStatus: Boolean, aligment: Alignment.Horizontal) {
+    Column(
+        modifier = Modifier.padding(8.dp),
+        horizontalAlignment = aligment,
+    ) {
         Text(
             text = userName,
             style = MaterialTheme.typography.titleLarge,
-            modifier = if (onlineStatus) Modifier.alpha(1f) else
-                Modifier.alpha(ContentAlpha.medium),
+            modifier = if (onlineStatus) {
+                Modifier.alpha(1f)
+            } else {
+                Modifier.alpha(ContentAlpha.medium)
+            },
         )
         Text(
-            text = if (onlineStatus)
+            text = if (onlineStatus) {
                 "Active Now"
-            else
-                "Offline",
+            } else {
+                "Offline"
+            },
             modifier = Modifier.alpha(ContentAlpha.medium),
             style = MaterialTheme.typography.titleSmall
         )
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun UserProfileDetailsPreview() {
+    MyTheme(dynamicColor = false) {
+        UserPrifleDetailsScreen(userProfileList[0])
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun UserListPreview() {
     MyTheme(dynamicColor = false) {
-        MainScreen()
+        UserListScreen(userProfiles = userProfileList, null)
     }
 }
